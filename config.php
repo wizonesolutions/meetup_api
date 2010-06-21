31de32e05e61153ccd14b4ff0d051fe3cb63ef3a
Kevin Kaland <kevin@magic2>
2010-06-20 21:34:37 -0700

    Created configuration file.

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @file config.php
+ * @author Kevin Kaland <help [at] wizonesolutions [dot] com>
+ * This file contains global configuration parameters for the Meetup API PHP Client.
+ * You will not normally need to change most of these, and if you do, you probably know what you're doing anyway.
+ */
+
+define('API_URL', 'http://api.meetup.com/'); //Include the trailing slash
+
+
