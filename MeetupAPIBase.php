7093ad2eef9e6737b2c2f13a024696031e03157e
Kevin Kaland <kevin@magic2>
2010-07-04 17:05:04 -0700

    Reasonably functional revision of the Meetup API. Need to implement paging, and then I should be able to release an alpha.

--- MeetupAPIBase.php
+++ MeetupAPIBase.php
@@ -18,6 +18,7 @@ require_once(dirname(__FILE__) . '/config.php');
  */
 class MeetupAPIBase {
   protected $validFormats = array('json',
+      'json-alt',
       'xml',
       );
   protected $apiKey, $apiUrl, $format, $method, $pageSize, $numPages, $sortDesc, $query, $curl; //This should be set to the Meetup API method implemented by the child class.
@@ -27,6 +28,16 @@ class MeetupAPIBase {
     $this->method = $method;
     $this->apiUrl = API_URL;
     $this->format = API_FORMAT;
+    if ($this->format == 'json') {
+      //Choose the (hopefully) best json library available based on the PHP version
+      if (version_compare(phpversion(), '5.3') >= 0) {
+        //Do nothing
+      }
+      else {
+        //Use the JSON functionality that the XML-RPC library provides
+        $this->format = 'json-alt';
+      }
+    }
     $this->pageSize = API_PAGE_SIZE;
     $this->numPages = API_NUM_PAGES;
     $this->sortDesc = API_SORT_DESC;
@@ -59,28 +70,47 @@ class MeetupAPIBase {
     else return TRUE;
   }
 
-  function getResults() {
-    if (!$this->validateFormat()) return;
-    $responseData = $this->request();
+  function getResults($offset = 0, $responseData = NULL) {
+    if (!$this->validateFormat()) return FALSE;
+    if (!isset($responseData)) {
+      $responseData = $this->request($offset);
+    }
     if ($results = $this->parseResponse($responseData)) {
       // @todo CRIT: Implement paging here.
       return $results;
     }
     else return FALSE;
   }
+
+  function getRawResults($offset = 0) {
+    if (!$this->validateFormat()) return FALSE;
+    return $this->request($offset);
+  }
   
   function parseResponse($responseData) {
     switch($this->format) {
       case 'json':
         //TODO: This json_decode has a BIG limitation, but it is built into PHP and is thus preferred. However, I wouldn't mind offering a backup decoding function. Gotta save on that paging activity.
-        $json_response = json_decode($responseData);
-        return $json_response;
+        $jsonResponse = json_decode($responseData);
+        return $jsonResponse;
+        break;
+      case 'json-alt':
+        require_once(dirname(__FILE__) . '/libraries/xmlrpc/lib/xmlrpc.inc');
+        require_once(dirname(__FILE__) . '/libraries/xmlrpc/extras/jsonrpc/jsonrpc.inc');
+        require_once(dirname(__FILE__) . '/libraries/xmlrpc/extras/jsonrpc/json_extension_api.inc');
+        $decodeFunc = 'json_decode';
+        if (extension_loaded('json')) {
+          $decodeFunc = 'json_alt_decode';
+        }
+        $jsonResponse = $decodeFunc($responseData, FALSE, 1);
+        return $jsonResponse;
         break;
       case 'xml':
-        $xml_response_raw = simplexml_load_string($responseData);
+        // @todo MEDIUM: Fix problems with XML parsing...disabling for now
+        /*$xml_response_raw = simplexml_load_string($responseData);
         //Here goes nothing
         $xml_response = $this->parseXmlIntoObject($xml_response_raw);
-        return $xml_response;
+        return $xml_response;*/
         break;
       default:
         //We don't know how to execute this request, so return FALSE.
@@ -92,7 +122,7 @@ class MeetupAPIBase {
 
   /* @todo: Taking this XML at face value is a bad idea. It would be better to hard-map the tags and data hierarchy to expect, a bit like I do in Easy Populate Converter. Then I can traverse it without recursion and get around the hideous question of, "How do I tell if a child has children itself, and if those are part of a list of itself or simply child properties?" Granted, there are few child properties that actually themselves contain multiple results. In fact, I can only think of topics. So accomodating all children isn't really needed. I can abstract this a bit and make life easier. I can refactor it later.
 
-  THEREFORE, this XML parsing is probably not something that MeetupAPIBase should be doing at all. It doesn't have enough information about what to expect, and the whole point of this module is to present callers with nice, easy-to-use, CONSISTENT arrays of results, regardless of whether they were retrieved in JSON or XML.
+  THEREFORE, this XML parsing is probably not something that MeetupAPIBase should be doing at all. It might work, though...need to play with it a bit more.
 
   A bit to consider.
   */
@@ -101,16 +131,7 @@ class MeetupAPIBase {
     $valueCount = count($xmlObj);
     if ($valueCount) {
       foreach($xmlObj as $key => $value) {
-        if ($depth == 0 && $key == 'head') {
-          //This is just to make it match the JSON. Maybe I should change them both to something else though? @todo: Decide on that.
-          $useKey = 'meta';
-        }
-        elseif ($depth == 0 && $key == 'items') {
-          $useKey = 'results'; //again, make it match the JSON
-        }
-        else {
-          $useKey = $key;
-        }
+        $useKey = $key;
         $children = $value->children();
         if (count($children)) {
           foreach($children as $key2 => $value2) {
@@ -182,15 +203,29 @@ class MeetupAPIBase {
     curl_close($this->curl);
   }
 
-  protected function request() {
-    $this->initRequest();
+  protected function request($offset = 0) {
+    $this->initRequest($offset);
     $responseData = $this->execRequest();
     $this->closeRequest();
     return $responseData;
   }
 
   protected function getRequestUrl() {
-    return $this->apiUrl . $this->method . '.' . $this->format;
+    return $this->apiUrl . $this->method . '.' . $this->formatRequestName($this->format);
+  }
+
+  /**
+   * formatRequestName 
+   * When various implementations of a format exist, this helper function ensures the right name is still passed to the API. 
+   * @param mixed $formatName 
+   */
+  protected function formatRequestName($formatName) {
+    switch($formatName) {
+      case 'json-alt':
+        return 'json';
+      default:
+        return $formatName;
+    }
   }
 
   function setPageSize($pageSize) {
@@ -225,8 +260,26 @@ $test_key = '336b4270111f5f4ba65156511d1a3d';
 $muApi = new MeetupAPIBase($test_key, 'groups');
 $muApi->setQuery( array('zip' => '11211',
       'order' => 'ctime',) );
-$muApi->setPageSize(43);
+//$muApi->setPageSize(5);
+$muApi->setPageSize(68);
 $muApi->setSortDesc(TRUE);
-$muApi->setFormat('xml');
-krumo($muApi->getResults());
+//$results = $muApi->getRawResults(12);
+$results = $muApi->getRawResults(0);
+krumo($muApi->getResults(0, $results));
+krumo($results);
+switch(json_alt_last_error()) {
+  case JSON_ALT_ERROR_DEPTH:
+    echo ' - Maximum stack depth exceeded';
+    break;
+  case JSON_ALT_ERROR_CTRL_CHAR:
+    echo ' - Unexpected control character found';
+    break;
+  case JSON_ALT_ERROR_SYNTAX:
+    echo ' - Syntax error, malformed JSON';
+    break;
+  case JSON_ALT_ERROR_NONE:
+    echo ' - No errors';
+}
+//$muApi->setPageSize(65);
+//krumo($muApi->getRawResults());
 
