fb2cbd9cab2522e6972a30edbac11904b0cb3bfd
Kevin Kaland <kevin@wizonesolutions.com>
2010-07-04 21:42:35 -0700

    Updated API_FORMAT comment text.

--- config.php
+++ config.php
@@ -7,7 +7,7 @@
  */
 
 define('API_URL', 'http://api.meetup.com/'); //Include the trailing slash
-define('API_FORMAT', 'json'); //This is the default format that API calls will use. At this time, only JSON (json) and XML are implemented.
+define('API_FORMAT', 'json'); //This is the default format that API calls will use. At this time, only JSON (json) is fully implemented. XML implementation has started, but needs work, so it is disabled.
 define('API_PAGE_SIZE', 200); //The number of results to return. This number must not exceed your allowed number of results, which at the time of writing was 200 by default.
 
 /* Increase this if you want to get more results per request. Set this to 0 to get all available results on every request. You can also override this setting by calling $yourApiObject->setPageSize(<page size>); This is recommended to avoid rapid exhaustion of your API limit. You can page manually by calling $yourApiObject->request($offset), where $offset is the page to which you wish to jump.
@@ -15,3 +15,4 @@ define('API_PAGE_SIZE', 200); //The number of results to return. This number mus
 define('API_NUM_PAGES', 1);
 
 define('API_SORT_DESC', FALSE); //Set this to TRUE if you want your results in descending order by default. Note that some methods provide their own ordering capabilities.
+
